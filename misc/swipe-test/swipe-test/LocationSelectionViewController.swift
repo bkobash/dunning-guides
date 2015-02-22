e827c8e0a209c6cdeb8a27024df49d2142c08ce0
John Dunning <jdunning@yahoo-inc.com>
2015-02-21 16:08:31 -0800

    Added swipe-test.  ffs

---
+++ misc/swipe-test/swipe-test/LocationSelectionViewController.swift
@@ -0,0 +1,67 @@
+//
+//  LocationSelectionViewController.swift
+//  swipe-test
+//
+//  Created by yahoo on 2/21/15.
+//  Copyright (c) 2015 yahoo. All rights reserved.
+//
+
+import UIKit
+
+class LocationSelectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+	
+	let CellReuseIdentifier = "LocationCell"
+	
+	@IBOutlet weak var collectionView: UICollectionView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		collectionView.delegate = self
+		collectionView.dataSource = self
+		collectionView!.registerClass(LocationCardCell.self, forCellWithReuseIdentifier: CellReuseIdentifier)
+		
+		// Do any additional setup after loading the view.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+
+	func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+		//#warning Incomplete method implementation -- Return the number of sections
+		return 1
+	}
+
+	
+	func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		//#warning Incomplete method implementation -- Return the number of items in the section
+		return 10
+	}
+	
+	func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+
+		println(indexPath.row, CellReuseIdentifier)
+		
+		var cell = collectionView.dequeueReusableCellWithReuseIdentifier(CellReuseIdentifier, forIndexPath: indexPath) as LocationCardCell
+		
+println(cell)
+		
+		cell.nameLabel.text = "\(indexPath.row)"
+		
+		return cell
+	}
+	
+	/*
+	// MARK: - Navigation
+	
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+	// Get the new view controller using segue.destinationViewController.
+	// Pass the selected object to the new view controller.
+	}
+	*/
+	
+}
